4f890c864745ad34a03a3c162e7062ae5fb5c44b
sohail808 <sohailworks10@gmail.com>
2023-09-09 23:53:28 +0530

    Try

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,46 @@
+// import "./App.css";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Profile from "./pages/Profile";
+// import Dashboard from "./pages/Dashboard";
+// import Transaction from "./pages/Transaction";
+// import Income from "./pages/Income";
+// import Expense from "./pages/Expense";
+// import Contact from "./pages/Contact";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<Register />}></Route>
+//         <Route path="register" element={<Register />}></Route>
+//         <Route path="login" element={<Login />}></Route>
+//         <Route path="profile" element={<Profile />}></Route>
+//         <Route path="dashboard" element={<Dashboard />}></Route>
+//         <Route path="transactions" element={<Transaction />}></Route>
+//         <Route path="income" element={<Income />}></Route>
+//         <Route path="expenses" element={<Expense />}></Route>
+//         <Route path="contact" element={<Contact />}></Route>
+//         <Route path="logout" element={<Register />}></Route>
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Profile from "./pages/Profile";
-import Dashboard from "./pages/Dashboard";
-import Transaction from "./pages/Transaction";
-import Income from "./pages/Income";
-import Expense from "./pages/Expense";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Register />}></Route>
-        <Route path="register" element={<Register />}></Route>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="profile" element={<Profile />}></Route>
-        <Route path="dashboard" element={<Dashboard />}></Route>
-        <Route path="transactions" element={<Transaction />}></Route>
-        <Route path="income" element={<Income />}></Route>
-        <Route path="expenses" element={<Expense />}></Route>
-        <Route path="contact" element={<Contact />}></Route>
-        <Route path="logout" element={<Register />}></Route>
-      </Routes>
+        <h1>Hii</h1>
     </>
   );
 }
 
 export default App;
+
