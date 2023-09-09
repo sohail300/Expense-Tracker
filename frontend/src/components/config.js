bcf59addef22dbd8fdde5a6749fc20b5f5e3600d
sohail808 <sohailworks10@gmail.com>
2023-09-09 23:39:10 +0530

    Link updated

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,4 +1,4 @@
-// const baseUrl = "https://expense-tracker-avsv.onrender.com";
-const baseUrl = "http://127.0.0.1:5000";
+const baseUrl = "https://expense-tracker-avsv.onrender.com";
+// const baseUrl = "http://127.0.0.1:5000";
 
 export default baseUrl
